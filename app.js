e2c0d3ffb2cd838a194fb5653b59bbd48ca69965
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-04-21 17:09:02 -0500

    Agregar botones menú y añadir estructura de carpetas

---
+++ app.js
@@ -0,0 +1,3 @@
+import menu from "./vistas/menu.js";
+
+menu()
---
+++ views/menu.js
@@ -0,0 +1,36 @@
+
+const createButton = (id, value, position) => {
+  console.log
+  let thisBtn = document.createElement("input");
+  thisBtn.setAttribute("type", "button");
+  thisBtn.setAttribute("id", id);
+  thisBtn.setAttribute("value", value);
+  document.getElementById(position).appendChild(thisBtn);
+  return thisBtn;
+};
+
+const menu = () => {
+  //play button
+  createButton("playBtn", "Jugar", "top")
+
+  // records button
+  createButton("recordsBtn", "Historial", "middle")
+
+  // add a question
+  createButton("addQuestionBtn", "Añadir pregunta", "bottom")
+
+  // events
+  // playBtn.addEventListener("click", () => {
+  //   console.log("Jugar");
+  // });
+
+  // recordsBtn.addEventListener("click", () => {
+  //   console.log("Historial");
+  // });
+
+  // addQuestionBtn.addEventListener("click", () => {
+  //   console.log("Agregar preguntas");
+  // });
+};
+
+export default menu;
