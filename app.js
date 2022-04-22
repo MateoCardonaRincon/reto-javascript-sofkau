fbb8a8c39bc997560babda400245e6b680a11f0e
MateoCardonaRincon <mcardonari@unal.edu.co>
2022-04-21 22:34:58 -0500

    Agregar tabla de historial de resultados
    Se hace un llamado a la lista de objetos almacenados en el localStorage y se agregan en una nueva fila con un <tr> y un <td>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import menu from "./views/menu.js";
-//import QUESTIONS_C1 from "/questions.js";
+import QUESTIONS_C1 from "/questions.js";
 
 menu();
 
-// localStorage.setItem("Prueba",  JSON.stringify(QUESTIONS_C1));
+localStorage.setItem("Prueba",  JSON.stringify(QUESTIONS_C1));
 // JSON.parse(localStorage.getItem("Prueba"))
--- modules/showRecords.js
+++ modules/showRecords.js
@@ -1,11 +1,21 @@
+var bdiv = document.querySelector("#body-div");
+
 const showRecords = () => {
-    let data = JSON.parse(localStorage.getItem("Prueba"))
+    let data = JSON.parse(localStorage.getItem("Prueba"));
 
-    let table = document.createElement("table")
-    table.setAttribute("id", "records-table")
-    let th = document.createElement("th")
+    var table = document.createElement("table");
+    table.setAttribute("id", "records-table");
 
-    document.body.innerHTML = `<h1>Historial</h1>`
+    let theader = `<table class="table table-striped table-bordered"><tr><th>Pregunta</th><th>Id</th></tr>`;
+
+    let tbody = ``
+    data.map((record) => {
+        tbody += `<tr><td>${record.question}</td>
+      <td>${record.id}</td></tr>`;
+    });
+
+
+    bdiv.innerHTML = theader.concat(tbody)
 };
 
 export default showRecords;
