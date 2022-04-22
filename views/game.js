0f646c4b7209bf4c33e4051268cda7bcab1fed6d
luis <luisfelorivas@gmai.com>
2022-04-22 00:37:11 -0500

    add the funtionalityof create a player
    an object player that allow to save the winners score was created

--- views/game.js
+++ views/game.js
@@ -1,4 +1,4 @@
-import {getQuestionObject, getChoice, getQuestion } from "./questionQuery.js"
+import { getQuestionObject, getChoice, getQuestion } from "./questionQuery.js";
 
 let game_init = async () => {
   let btn_init_game = document.createElement("input");
@@ -11,43 +11,38 @@ let game_init = async () => {
   //Set  name input
   name_input.type = "text";
   name_input.placeholder = "nombre jugador";
+  let name = await name_input.value;
+
+  let player = {
+    name: "luis",
+    score: 0,
+  };
 
   //init the round
   btn_init_game.addEventListener("click", () => {
-    initRound(0);
+    initRound(0, player);
   });
 
   document.getElementById("top").appendChild(name_input);
   document.getElementById("top").appendChild(btn_init_game);
 };
 
-
-
 // Init a new round
-async function initRound(index_round) {
+async function initRound(index_round, player) {
   document.getElementById("middle").innerHTML = "";
   let question_div = document.createElement("div");
   let answer_div = document.createElement("div");
   let table = document.createElement("table");
+  let number_of_questions = 4;
+
+  console.log(player);
 
   //generate a random index of a question
-  let index_question = getRandomInt(0, 3);
+  let index_question = getRandomInt(0, number_of_questions - 1);
 
   //set a question
   question_div.innerHTML = await getQuestion(index_round, index_question);
-
-  //set posible answer to the question
-  for (let index_choice = 0; index_choice < 4; index_choice++) {
-    let answ = await getChoice(index_question, index_choice);
-    let row = document.createElement("tr");
-    let collum = document.createElement("tb");
-    collum.innerHTML = answ;
-    row.appendChild(collum);
-    row.addEventListener("click", () =>
-      checkAnswer(index_question, index_choice, index_round + 1)
-    );
-    table.appendChild(row);
-  }
+  showAnswers(index_question, index_round, table, player);
   answer_div.appendChild(table);
 
   document.getElementById("middle").appendChild(question_div);
@@ -55,6 +50,20 @@ async function initRound(index_round) {
   document.getElementById("middle").appendChild(answer_div);
 }
 
+//show the posible answers to the question
+async function showAnswers(index_question, index_round, table, player) {
+  for (let index_choice = 0; index_choice < 4; index_choice++) {
+    let answ = await getChoice(index_question, index_choice);
+    let row = document.createElement("tr");
+    let collum = document.createElement("tb");
+    collum.innerHTML = answ;
+    row.appendChild(collum);
+    row.addEventListener("click", () =>
+      checkAnswer(index_question, index_choice, index_round + 1, player)
+    );
+    table.appendChild(row);
+  }
+}
 
 // check if an answer is correct
 async function isCorrect(index_question, index_choice) {
@@ -65,9 +74,8 @@ async function isCorrect(index_question, index_choice) {
   return is_correct;
 }
 
-
 //check if an answer is correct for a question
-async function checkAnswer(index_question, index_choice, level) {
+async function checkAnswer(index_question, index_choice, level, player) {
   let answer = await isCorrect(index_question, index_choice);
   if (answer) {
     console.log("check");
