f73a81ff0c7ee0b28202923b505e3d269f9a5b29
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-04-21 19:21:14 -0500

    Se modifico questions.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 import menu from "./views/menu.js";
+import {
+  questions_c1,
+  questions_c2,
+  questions_c3,
+  questions_c4,
+  questions_c5,
+} from "./persistencia/questions.js";
 
-menu()
+menu();
