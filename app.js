03b13a545ad5bc059b1ae9f343dd5e80d3f6de85
Mateo Cardona Rincón <76075911+MateoCardonaRincon@users.noreply.github.com>
2022-04-23 20:45:53 -0500

    Update app.js
    Change the order of localStorage() and manu() function call, and remove comments

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import menu from "./views/menu.js";
 import QUESTIONS_C1 from "/questions.js";
 
+localStorage.setItem("QuestionsC1", JSON.stringify(QUESTIONS_C1));
+
 menu();
 
-localStorage.setItem("QuestionsC1", JSON.stringify(QUESTIONS_C1));
-// JSON.parse(localStorage.getItem("Prueba"))
